e810cfc0b619e2eeb50427918e8cd04b8781de92
Lethokuhle Shandu <Lethokuhle.Shandu@studentambassadors.com>
2022-10-28 13:54:37 +0200

    - Changes to GamersMed Title to white -Changes to banner1 image contrast and brightness - Changes Contact Tab on Menu - Changes to email address

--- PBDE401/Migrations/202210281117533_Creation.cs
+++ PBDE401/Migrations/202210281117533_Creation.cs
@@ -3,16 +3,14 @@
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class RemoveRequired : DbMigration
+    public partial class Creation : DbMigration
     {
         public override void Up()
         {
-            AlterColumn("dbo.Medicines", "ImageUrl", c => c.String());
         }
         
         public override void Down()
         {
-            AlterColumn("dbo.Medicines", "ImageUrl", c => c.String(nullable: false));
         }
     }
 }
